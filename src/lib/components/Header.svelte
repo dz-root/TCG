16e1a0989d16012eee789250b775a5392cd81946
Kingweb <dz-root@hotmail.fr>
2025-10-28 17:52:10 +0100

    Refont menu

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,6 +4,6 @@
 </script>
 <p class="max-w-6xl mx-auto text-white px-5 py-2 font-bold">{title}</p>
 <div class="flex gap-5 max-w-6xl mx-auto text-[#8f93a2] px-5 font-semibold">
-    <a href="/" class="hover:text-blue-200" class:text-blue-300={$page.url.pathname === '/'}>POC</a>
-    <a href="/doc" class="hover:text-blue-200" class:text-blue-300={$page.url.pathname === '/doc'}>DOC</a>
+    <a href="/" class="hover:text-blue-200" class:text-blue-300={$page.url.pathname === '/'}>[POC]</a>
+    <a href="/doc" class="hover:text-blue-200" class:text-blue-300={$page.url.pathname === '/doc'}>[DOC]</a>
 </div>
